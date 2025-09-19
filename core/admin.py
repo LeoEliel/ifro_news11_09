487e2579bf466987fed03f2f4744868e365a2151
Leonardo Eliel Dias da Silva <2024105100032@aluno.ifro.edu.br>
2025-09-18 21:27:58 -0400

    terceiro commit

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Cargo, Funcionario
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'descricao']
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'cargo', 'ativo', 'criado']
+    list_editable = ['ativo', '']
--- core/models.py
+++ core/models.py
@@ -1,3 +1,48 @@
+from email.policy import default
+
 from django.db import models
+from stdimage import StdImageField
 
 # Create your models here.
+
+class Base(models.Model):
+    criado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
+    ativo = models.BooleanField(default=True)
+
+
+class Meta:
+    abstract = True
+
+class Cargo(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=200,
+                                 blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
+
+class Funcionario(Base):
+    nome = models.CharField(max_length=100)
+    bio = models.TextField()
+    cargo = models.ForeignKey(Cargo, on_delete=models.SET_NULL,
+                              blank=True, null=True)
+    foto = StdImageField(
+        upload_to='equipe',
+        variations=
+         {
+             'thumb':
+             {
+                 'width': 60,
+                 'height': 60,
+                 'crop':True
+            }
+         }
+    )
+
+class Meta:
+    verbose_name = 'Funcionario'
+    verbose_name_plural = 'Funcionarios'
+
+def __str__(self):
+    return self.nome
